9d01621c5c07ea136dfc7e81dbdda98240cb3b03
alex1651 <aleks.16541@yandex.ru>
2023-03-01 10:33:27 +0300

    + json-server

---
+++ json-server/index.js
@@ -0,0 +1,51 @@
+// server.js
+const jsonServer = require('json-server');
+const path = require('path');
+const fs = require('fs');
+
+const server = jsonServer.create();
+const router = jsonServer.router(path.resolve(__dirname, 'db.json'));
+
+server.use(jsonServer.defaults({}));
+server.use(jsonServer.bodyParser);
+
+function isAuthorized(req) {
+    return !!req.headers.authorization;
+}
+
+server.use(async (req, res, next) => {
+    // eslint-disable-next-line no-promise-executor-return
+    await new Promise((res) => setTimeout(res, 1000));
+    next();
+});
+
+server.post('/login', (req, res) => {
+    console.log(req.body);
+    const { username, password } = req.body;
+    const db = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'db.json'), 'UTF-8'));
+    const { users } = db;
+
+    const userFromDb = users.find(
+        (user) => user.username === username && user.password === password,
+    );
+
+    if (userFromDb) {
+        return res.json(userFromDb);
+    }
+
+    return res.status(403).json({ massage: 'AUTH ERROR' });
+});
+
+server.use((req, res, next) => {
+    if (isAuthorized(req)) {
+        next();
+    } else {
+        res.sendStatus(401);
+    }
+});
+
+server.use(router);
+
+server.listen(3001, () => {
+    console.log('JSON Server is running on 3001 port');
+});
