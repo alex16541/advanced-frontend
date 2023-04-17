57c8e3610806502bfa4cb83dc0709de048e970ab
dovidenkov <dovidenkov@reshenie-soft.ru>
2023-04-17 16:54:21 +0300

    Visual changes + icons ~ themeSwitcher icon chage

--- src/widgets/ThemeSwitcher/ui/ThemeSwitcher.tsx
+++ src/widgets/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -1,7 +1,6 @@
 import { classNames } from 'shared/lib/classNames/classNames';
-import { Theme, useTheme } from 'app/providers/ThemeProvider';
-import LightIcon from 'shared/assets/svg/sun.svg';
-import DarkIcon from 'shared/assets/svg/moon.svg';
+import { useTheme } from 'app/providers/ThemeProvider';
+import PaintRoller from 'shared/assets/svg/paint-roller.svg';
 import { Button, ButtonThemes } from 'shared/ui/Button';
 import { FC, memo } from 'react';
 import cls from './ThemeSwitcher.module.scss';
@@ -12,7 +11,7 @@ interface ThemeSwitcherProps {
 
 export const ThemeSwitcher: FC<ThemeSwitcherProps> = memo((props: ThemeSwitcherProps) => {
     const { className, ...otherProps } = props;
-    const { theme, toggleTheme } = useTheme();
+    const { toggleTheme } = useTheme();
 
     return (
         <Button
@@ -21,7 +20,7 @@ export const ThemeSwitcher: FC<ThemeSwitcherProps> = memo((props: ThemeSwitcherP
             className={classNames(cls.ThemeSwitcher, {}, [className])}
             {...otherProps}
         >
-            {theme === Theme.DARK ? <DarkIcon /> : <LightIcon />}
+            <PaintRoller />
         </Button>
     );
 });
