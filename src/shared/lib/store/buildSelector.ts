5fc6c24e151434b2b373edfa9e5b61f9d17b7ee7
Алексей Довыденков <aleks.16541@yandex.ru>
2024-03-24 14:15:35 +0300

    Upgrade buildSelector

--- src/shared/lib/store/buildSelector.ts
+++ src/shared/lib/store/buildSelector.ts
@@ -1,11 +1,13 @@
 import { StateSchema } from '@/app/providers/StoreProvider';
 import { useAppSelector } from '@/shared/hooks/useAppSelector';
 
-type Selector<T> = (store: StateSchema) => T;
-type Result<T> = [() => T, Selector<T>];
+type Selector<T, Args extends any[]> = (store: StateSchema, ...args: Args) => T;
+type Hook<T, Args extends any[]> = (...args: Args) => T;
+type Result<T, Args extends any[]> = [Hook<T, Args>, Selector<T, Args>];
 
-export function buildSelector<T>(selector: Selector<T>): Result<T> {
-    const useSelectorHook = () => useAppSelector(selector);
+export function buildSelector<T, Args extends any[]>(selector: Selector<T, Args>): Result<T, Args> {
+    const useSelectorHook = (...args: Args) =>
+        useAppSelector((state: StateSchema) => selector(state, ...args));
 
     return [useSelectorHook, selector];
 }
