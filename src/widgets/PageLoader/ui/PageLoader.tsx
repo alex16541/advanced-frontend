cf91e327e1db8c03182f0ee05c01980f8e1297be
Алексей Довыденков <aleks.16541@yandex.ru>
2024-04-17 19:19:50 +0300

    Redesign PageLoader

--- src/widgets/PageLoader/ui/PageLoader.tsx
+++ src/widgets/PageLoader/ui/PageLoader.tsx
@@ -1,8 +1,12 @@
 import { useTranslation } from 'react-i18next';
 
 import { classNames } from '@/shared/lib/classNames/classNames';
+import { FeatureToggle } from '@/shared/lib/features/components/FeatureToggle/FeatureToggle';
 import { Loader } from '@/shared/ui/deprecated/Loader';
 import { Text } from '@/shared/ui/deprecated/Text';
+import { Card } from '@/shared/ui/redesigned/Card';
+import { Skeleton } from '@/shared/ui/redesigned/Skeleton';
+import { VStack } from '@/shared/ui/redesigned/Stack';
 
 import cls from './PageLoader.module.scss';
 
@@ -16,9 +20,32 @@ export const PageLoader = (props: PageLoaderProps) => {
     const { t } = useTranslation();
 
     return (
-        <div className={classNames(cls.PageLoader, {}, [className])}>
-            <Loader />
-            <Text text={text || t('page loader text')} />
-        </div>
+        <FeatureToggle
+            feature="isRedesignedApp"
+            off={
+                <div className={classNames(cls.PageLoaderDeprecated, {}, [className])}>
+                    <Loader />
+                    <Text text={text || t('page loader text')} />
+                </div>
+            }
+            on={
+                <Card className={classNames(cls.PageLoader, {}, [className])}>
+                    <VStack gap="20">
+                        <Skeleton height="38px" width="40%" />
+                        <VStack gap="16">
+                            <Skeleton height="24px" width="100%" />
+                            <Skeleton height="24px" width="100%" />
+                            <Skeleton height="24px" width="100%" />
+                            <Skeleton height="24px" width="100%" />
+                            <Skeleton height="24px" width="100%" />
+                            <Skeleton height="24px" width="100%" />
+                            <Skeleton height="24px" width="100%" />
+                            <Skeleton height="24px" width="100%" />
+                            <Skeleton height="24px" width="30%" />
+                        </VStack>
+                    </VStack>
+                </Card>
+            }
+        />
     );
 };
