d054f9c8b823806ea4740e6bc8993bd1bd3fd68f
Алексей Довыденков <aleks.16541@yandex.ru>
2024-09-17 23:22:42 +0300

    Create welcome message on main page

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,27 +1,32 @@
 import { useTranslation } from 'react-i18next';
 
 // eslint-disable-next-line alex16541-fsd-imports-path-checker/layers-imports
-import { BugButton } from '@/app/providers/ErrorBoundary';
-import { Counter } from '@/entity/Counter';
-import { getFeatureFlag } from '@/shared/lib/features';
-import { Text } from '@/shared/ui/deprecated/Text';
+import { StikyContentLayout } from '@/shared/layouts/StikyContentLayout';
+import { Card } from '@/shared/ui/redesigned/Card';
+import { Text } from '@/shared/ui/redesigned/Text';
 import { Page } from '@/widgets/Page';
+
+import cls from './MainPage.module.scss';
 
 const MainPage = () => {
     const { t } = useTranslation();
-    const isCounterEnables = getFeatureFlag('isCounterEnabled');
-
     return (
         <Page className="page" data-testid="main-page">
-            <div className="title">{t('main page title')}</div>
-            <Text
-                text="test Lorem ipsum dolor
-                sit amet consectetur adipisicing
-                elit. Numquam, quidem?"
+            <StikyContentLayout
+                content={
+                    <Card className={cls.welcomeCard}>
+                        <Text title={t('welcome title')} />
+                        <br />
+                        <Text text={t('welcome text 1')} />
+                        <br />
+                        <Text text={t('welcome dot 1')} />
+                        <Text text={t('welcome dot 2')} />
+                        <Text text={t('welcome dot 3')} />
+                        <br />
+                        <Text text={t('welcome text 2')} />
+                    </Card>
+                }
             />
-            <BugButton />
-
-            {isCounterEnables && <Counter />}
         </Page>
     );
 };
