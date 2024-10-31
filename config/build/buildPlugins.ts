05bfabfa6810c7352bd511629caa0547e80b3aef
Алексей Довыденков <aleks.16541@yandex.ru>
2024-10-31 17:37:40 +0300

    Fix: CI\CD

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,6 +1,5 @@
 import ReactRefreshPlugin from '@pmmmwh/react-refresh-webpack-plugin';
 import CircularDependencyPlugin from 'circular-dependency-plugin';
-import CopyPlugin from 'copy-webpack-plugin';
 import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
@@ -41,11 +40,6 @@ function buildPlugins({
         plugins.push(new MiniCssExtractPlugin({
             filename: 'css/[name].[contenthash:8].css',
             chunkFilename: 'css/[name].[contenthash:8].css',
-        }));
-        plugins.push(new CopyPlugin({
-            patterns: [
-                { from: 'public/locales', to: 'locales' },
-            ],
         }));
     }
 
